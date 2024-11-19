61df86592af93318a8189055c52016f81a05dd8c
Edwin Hernandez Campos <edwincampos803@gmail.com>
2024-11-19 15:17:29 -0600

    Practica 12
    Se agregaron cambios

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./views/**/*.pug'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: '#FFFFFF', // Blanco
+        black: '#000000', // Negro
+        blue: '#95B4D5', // Azul claro
+        red: '#F75555', // Rojo
+        green: '#93E16A', // Verde
+      },
+    },
   },
   plugins: [],
 }
