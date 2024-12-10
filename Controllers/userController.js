a4af0e7c834f212f5ecbf87e52b30b8db0480d8c
Edwin Hernandez Campos <230425@utxicotepec.edu.mx>
2024-12-10 15:57:26 -0600

    Practica 13
    Implementación de recupueración de contraseñas

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -2,7 +2,7 @@ import { check, validationResult } from "express-validator"
 
 import User from "../Models/Users.js"
 import { generateId } from "../helpers/tokens.js"
-import { registerEmail } from '../helpers/emails.js'
+import { registerEmail, passwordRecoveryEmail } from '../helpers/emails.js'
 
 const formularioLogin = (req, res) => {
     res.render('auth/login', {
@@ -47,7 +47,7 @@ const register = async (req, res) => {
         return res.render('auth/register', {
             page: 'Crear Cuenta',
             csrfToken: req.csrfToken(),
-            errores: result.array(),
+            errors: result.array(),
             user: {
                 nombre: req.body.nombre,
                 email: req.body.email,
@@ -66,7 +66,7 @@ const { nombre, email, password, birthDate} = req.body
         return res.render('auth/register', {
             page: 'Crear Cuenta',
             csrfToken: req.csrfToken(),
-            errores: [{msg: 'El usuario ya existe'}],
+            errors: [{msg: 'El usuario ya existe'}],
             user: {
                 nombre: req.body.nombre,
                 email: req.body.email,
@@ -94,7 +94,7 @@ const { nombre, email, password, birthDate} = req.body
 
 
     // ? Mostrar mensaje de confirmacion
-    res.render('templates/mesage', {
+    res.render('templates/message', {
         page: 'Cuenta Creada Correctamente', 
         mesage: `Se ha enviado un email de confirmación a: ${req.body.email}, por favor, ingrese al siguiente enlace`
     })
@@ -135,6 +135,138 @@ const formularioPasswordRecovery = (req, res) => {
     })
 }
 
+// Validar token para restablecimiento de contraseña
+const checkToken = async (req, res) => {
+    const { token } = req.params;
+  
+    const user = await User.findOne({ where: { token } });
+  
+    if (!user || !user.confirm) {
+      return res.render("auth/confirmAccount", {
+        page: "Restablece tu Contraseña...",
+        msg: "Hubo un error al validar tu información. Verifica que tu cuenta esté confirmada.",
+        error: true,
+      });
+    }
+  
+    // Formulario para modificar el password
+    res.render("auth/reset-password", {
+      page: "Restablece tu Contraseña",
+      csrfToken: req.csrfToken(),
+    });
+  };
+  const resetPassword = async (req, res) => {
+    await check('email').notEmpty().withMessage('El correo electrónico es un campo obligatorio')
+      .isEmail().withMessage('El correo electrónico no tiene el formato correcto')
+      .run(req);
+  
+    const resultado = validationResult(req);
+  
+    if (!resultado.isEmpty()) {
+      return res.render('auth/passwordRecovery', {
+        page: 'Recupera tu acceso a Bienes Raíces',
+        csrfToken: req.csrfToken(),
+        errors: resultado.array(),
+      });
+    }
+  
+    const { email } = req.body;
+  
+    // Buscar el usuario
+    const user = await User.findOne({ where: { email } });
+  
+    if (!user || !user.confirm) {
+      return res.render('auth/passwordRecovery', {
+        page: 'Recupera tu acceso a Bienes Raíces',
+        csrfToken: req.csrfToken(),
+        errors: [{ msg: 'El correo no pertenece a un usuario confirmado.' }],
+      });
+    }
+  
+    // Generar un token
+    user.password="";
+    user.token = generateId();
+    await user.save();
+  
+    // Enviar un Email
+    passwordRecoveryEmail({
+      email: user.email,
+      nombre: user.nombre,
+      token: user.token,
+    });
+  
+    // Mostrar mensaje de confirmación
+    res.render('templates/message', {
+      page: 'Restablece tu Contraseña',
+      msg: `Hemos enviado un email a ${email} con las instrucciones para restablecer tu contraseña.`,
+    });
+  };
+  
+  // Restablecer contraseña
+  const newPassword = async (req, res) => {
+    // Validar el password
+    await check("new_password")
+      .notEmpty().withMessage("La contraseña es un campo obligatorio")
+      .isLength({ min: 8 }).withMessage("El Password debe ser de al menos 8 caracteres")
+      .run(req);
+    await check("new_password2")
+      .equals(req.body.new_password).withMessage("La contraseña debe coincidir con la anterior")
+      .run(req);
+  
+    const resultado = validationResult(req);
+  
+    // Verificamos que el resultado esté vacío
+    if (!resultado.isEmpty()) {
+      return res.render("auth/reset-password", {
+        page: "Reestablece tu Contraseña",
+        csrfToken: req.csrfToken(),
+        errors: resultado.array(),
+      });
+    }
+    const { token } = req.params;
+    const { new_password } = req.body;
+  
+    // Identificar quién hace el cambio
+    const user = await User.findOne({ where: { token } });
+  
+    // Hashear el nuevo password
+    user.password = new_password;
+    user.token = null;
+    await user.save();
+  
+    res.render("auth/confirmAccount", {
+      page: "Password Reestablecido",
+      msg: "El password se Guardó correctamente ",
+    });
+  };
+  
+  // Confirmar operación adicional
+  const confirm = async (req, res) => {
+    const { token } = req.params;
+    const user = await User.findOne({ where: { token } });
+  
+    if (!user) {
+      return res.render("auth/confirmAccount", {
+        page: "Error",
+        msg: "Token inválido",
+        error: true,
+      });
+    }
+  
+    res.render("templates/message", {
+      page: "Operación Confirmada",
+      msg: `Operación confirmada para el usuario ${user.name}.`,
+    });
+  };
+
 export {
-    formularioLogin, formularioRegister, register, confirmAccount, formularioPasswordRecovery
+    formularioLogin,
+    formularioRegister,
+    formularioPasswordRecovery, 
+    register, 
+    confirmAccount,
+    resetPassword,
+    confirm,
+    checkToken,
+    newPassword,    
 }
--- Models/users.js
+++ Models/users.js
@@ -1,33 +1,39 @@
-import { DataTypes } from "sequelize";
-import bcrypt from 'bcrypt'
-import db from "../db/config.js";
+    import { DataTypes } from "sequelize";
+    import bcrypt from 'bcrypt'
+    import db from "../db/config.js";
 
-const User =  db.define('Users', {
-    nombre: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    birthDate: { 
-        type: DataTypes.DATE,
-        allowNull: true, 
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    token: DataTypes.STRING,
-    confirm: DataTypes.BOOLEAN,
-}, {
-    hooks: {
-        beforeCreate: async function(user) {
-            const salt = await bcrypt.genSalt(10)
-            user.password = await bcrypt.hash(user.password, salt)
+    const User =  db.define('Users', {
+        nombre: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        birthDate: { 
+            type: DataTypes.DATEONLY,
+            allowNull: false, 
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        token: DataTypes.STRING,
+        confirm: DataTypes.BOOLEAN,
+    },{
+        hooks: {
+            beforeCreate: async (user) => {
+                const salt = await  bcrypt.genSalt(10)
+                user.password = await bcrypt.hash(user.password, salt);
+            },
+            beforeUpdate: async (user) =>
+            {
+                if (user.password && user.password.trim() != ''){
+                    user.password = await bcrypt.hash(user.password, 10);
+                }
+            }
         }
-    }
-})
+    })  
 
-export default User
+    export default User
